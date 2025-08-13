0aff249f36a664a59617ffd6ce53081ac7d0fc63
Rheetham Menon <rheethammenon@gmail.com>
2025-08-13 18:53:31 +0530

    Created a new spring boot project

---
+++ src/main/java/com/rheetham/primeplanner/PrimeplannerApplication.java
@@ -0,0 +1,13 @@
+package com.rheetham.primeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rheetham/primeplanner/PrimeplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rheetham.primeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
