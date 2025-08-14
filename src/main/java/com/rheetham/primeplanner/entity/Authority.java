f7079f6624f67d63228510bc63b1d47f6e78505b
Rheetham Menon <rheethammenon@gmail.com>
2025-08-14 19:27:00 +0530

    checkpoint: Created Todo, User and Authority entities

---
+++ src/main/java/com/rheetham/primeplanner/entity/Authority.java
@@ -0,0 +1,23 @@
+package com.rheetham.primeplanner.entity;
+
+import jakarta.persistence.Embeddable;
+import org.springframework.security.core.GrantedAuthority;
+
+@Embeddable
+public class Authority implements GrantedAuthority {
+
+    private String authority;
+
+    public Authority() {
+
+    }
+
+    public Authority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+}
